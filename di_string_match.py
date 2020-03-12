24aaccc45834db826b503467fd38ae4fa99ab357
Abel Joseph <38389364+abeljoseph@users.noreply.github.com>
2020-03-12 15:49:20 -0400

    Create di_string_match.py

---
+++ di_string_match.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def diStringMatch(self, S):
+        """
+        :type S: str
+        :rtype: List[int]
+        """
+        a, b = 0, len(S) # creates pointers
+        result = []
+        for i in S:  # looking at each char, either append the the low or high pointer value, then increment or decrement
+            if i=="I":
+                result.append(a) 
+                a+=1
+            if i =="D":
+                result.append(b)
+                b-=1
+        result.append(a)  # append the last digit that's left over (can be either a or b)
+        return result
+            
