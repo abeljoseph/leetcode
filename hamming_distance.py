6034b202fc9e59e1319c79503184387eb2a20a37
Abel Joseph <38389364+abeljoseph@users.noreply.github.com>
2020-03-13 00:28:16 -0400

    Update hamming_distance.py

--- hamming_distance.py
+++ hamming_distance.py
@@ -1,1 +1,11 @@
-
+class Solution:
+    def hammingDistance(self, x: int, y: int) -> int:
+        bin_x = f"{x:031b}"
+        bin_y = f"{y:031b}"
+        
+        h_dist = 0
+        for i, letter in enumerate(bin_x):
+            if not letter == bin_y[i]:
+                h_dist += 1
+                
+        return h_dist
